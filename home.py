40e64d3f8dd8b68c28fb69519729a258a3ff54ce
Evandro Anholeto <evandroanholeto@gmail.com>
2026-02-22 14:21:40 -0300

    Commit
    Commit.

--- home.py
+++ home.py
@@ -3,15 +3,15 @@ import streamlit as st
 st.set_page_config(page_title="Data Analytics - Turma 10DTAT - Grupo 56", layout="wide")
 
 pg = st.navigation([
-    st.Page("introducao.py", title="Introdução ao Tech Challenge 4", icon="📖"),
-    st.Page("metricasdeobesidade.py", title="Métricas de Obesidade (OMS)", icon="⚕️"),
+    st.Page("introducao.py", title="Introdução", icon="📖"),
+    st.Page("oms.py", title="Obesidade e seus Fatores (OMS)", icon="⚕️"),
     st.Page("analisexploratoriadedados.py", title="Analise Exploratória de Dados (EDA)", icon="📊"),
     st.Page("modelopreditivo.py", title="Modelo Preditivo", icon="💻"),
     st.Page("conclusao.py", title="Conclusão", icon="🎯")
 ])
 
 with st.sidebar:
-    st.markdown("<h3 style='margin-bottom: 0px; padding-bottom: 0px;'>Data Analytics - Turma 10DTAT - Grupo 56</h3>", unsafe_allow_html=True)
+    st.markdown("<h3 style='margin-bottom: 0px; padding-bottom: 0px;'>Data Analytics - Turma 10DTAT - Grupo 56 - Tech Challenge 4</h3>", unsafe_allow_html=True)
     st.divider()
     st.markdown("""
     *Evandro Anholeto*  
